c61923c3bdda4a03a2b5a149055809d93059809d
Har1s-Akbar <stanker801@gmail.com>
2023-09-28 13:15:38 -0700

    creating the project

---
+++ app/_components/Cover.jsx
@@ -0,0 +1,50 @@
+'use client'
+
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import Image from 'next/image';
+
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+function Slider() {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty('--progress', 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
+  return (
+    <main className=''>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        <SwiperSlide><Image src='https://images.unsplash.com/photo-1695575161610-7aeb03933996?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80' alt='img' width={1000} height={500} /></SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
+      </Swiper>
+    </main>
+  );
+}
+
+export default Slider
---
+++ app/_components/Nav.jsx
@@ -0,0 +1,85 @@
+"use client"
+
+import React from 'react'
+import { Separator } from '@/components/ui/separator'
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+import Link from 'next/link'
+
+
+function Nav() {
+  return (
+    <main className='w-full border-y-2 py-3 mt-10 shadow-md border-2 flex items-center justify-center'>
+        <NavigationMenu>
+          <NavigationMenuList>
+              <NavigationMenuItem className='hover:bg-secondary text-teal-700 transition duration-300 delay-300 ease-in-out py-2 px-4 rounded'>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink>
+                    Home
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className='hover:bg-secondary transition duration-300 text-teal-700 delay-300 ease-in-out py-2 px-4 rounded'>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink>
+                    About
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className='hover:bg-secondary transition duration-300 text-teal-700 delay-300 ease-in-out py-2 px-4 rounded'>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink>
+                    Genomics
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className='hover:bg-secondary transition duration-300 text-teal-700 delay-300 ease-in-out py-2 px-4 rounded'>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink>
+                    Biocomputomic
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className='hover:bg-secondary transition duration-300 text-teal-700 delay-300 ease-in-out py-2 px-4 rounded'>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink>
+                    Matagenomics
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className='hover:bg-secondary transition duration-300 text-teal-700 delay-300 ease-in-out py-2 px-4 rounded'>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink>
+                    Protemics
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className='hover:bg-secondary transition duration-300 text-teal-700 delay-300 ease-in-out py-2 px-4 rounded'>
+              <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink>
+                    Technical writing
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className='hover:bg-secondary transition duration-300 text-teal-700 delay-300 ease-in-out py-2 px-4 rounded'>
+              <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink>
+                    Contact
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+    </main>
+  )
+}
+
+export default Nav
